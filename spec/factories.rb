82dbbbb9abae095645b6b7cf5819f54da8557a1d
Jacek <jacek@jacek-laptop.(none)>
2013-02-03 20:08:05 +0100

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Jacek Jablonka"
-    email    "jacjab@gmail.com"
+    sequence(:name) 	{ |n| "Person #{n}" }
+    sequence(:email) 	{ |n| "person_#{n}@example.com" }
     password "qaz@1234"
     password_confirmation "qaz@1234"
+
+    factory :admin do
+    	admin true
+    end
   end
 end
